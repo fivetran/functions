c12cf17a85219836e8468e8605d3d25cfc1b2c1e
ParagPatil96 <parag@fivetran.com>
2018-06-26 12:50:45 +0530

    addressed comments

--- functions/Fetch_JSON_Data_from_PublicApi.js
+++ functions/Fetch_JSON_Data_from_PublicApi.js
@@ -10,9 +10,9 @@ function update(state, secret, callback) {
     let since = state.astroidsCursor || "2018-05-01";
     let baseApi = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + new Date(since).toISOString().slice(0,10) + "&api_key=" + secret.apiKey;
         const request = https.get(baseApi, response => {
-            if (response.statusCode < 200 || response.statusCode > 299) {
+            if (response.statusCode < 200 || response.statusCode > 299)
                 callback(new Error('Request failed with code: ' + response.statusCode), null);
-            }
+
             // temporary data holder
             let body = "";
             response.on('data', (chunk) => body += chunk);
@@ -20,17 +20,18 @@ function update(state, secret, callback) {
                 let insertAsteroids = [], deleteAsteroids = [];
                 let jsonData = JSON.parse(body);
                 let neoData = jsonData["near_earth_objects"];
-                let deleted = false;
+                let deleted = true;
                 for (var date in neoData) {
-                    if (neoData.hasOwnProperty(date)) {
-                        if(deleted){
-                            neoData[date].map(a => insertAsteroids.push(flattenOneLevel(a)));
-                        } else {
-                            neoData[date].map(a => deleteAsteroids.push(flattenOneLevel(a)));
-                            deleted = true;
-                        }
-                        since = (new Date(date) > new Date(since)) ? date : since;
+                    if (!neoData.hasOwnProperty(date))
+                        continue;
+
+                    if(deleted){
+                        neoData[date].map(a => deleteAsteroids.push(flattenOneLevel(a)));
+                        deleted = false;
+                    } else {
+                        neoData[date].map(a => insertAsteroids.push(flattenOneLevel(a)));
                     }
+                    since = (new Date(date) > new Date(since)) ? date : since;
                 }
                 
                 result.insert.astroids = insertAsteroids;
@@ -52,37 +53,36 @@ function flattenOneLevel(arr) {
 
 function flatten(currentKey, obj, target, nested) {
     for (var i in obj) {
-        if (obj.hasOwnProperty(i)) {
-            var newKey = i;
-            var newVal = obj[i];
+        if (!obj.hasOwnProperty(i)) 
+            continue;
+        
+        var newKey = i;
+        var newVal = obj[i];
 
-            if (currentKey.length > 0) {
-                newKey = currentKey + '_' + i;
-            }
+        if (currentKey.length > 0) 
+            newKey = currentKey + '_' + i;
 
-            if (nested && typeof newVal === "object") {
-                flatten(newKey, newVal, target, false);
-            } else {
-                target[newKey] = newVal;
-            }
-        }
+        if (nested && typeof newVal === "object") 
+            flatten(newKey, newVal, target, false);
+        else
+            target[newKey] = newVal;
     }
 }
 
 function initializeState(state) {
     return {
-                state: {...state},
-                insert: {
-                    "astroids": []
-                },
-                delete: {
-                    "astroids": []
-                },
-                schema : {
-                    "astroids" : {
-                        primary_key : ['neo_reference_id']
-                    }
-                },
-                hasMore : false
-            };
+        state: {...state},
+        insert: {
+            "astroids": []
+        },
+        delete: {
+            "astroids": []
+        },
+        schema : {
+            "astroids" : {
+                primary_key : ['neo_reference_id']
+            }
+        },
+        hasMore : false
+    };
 }
