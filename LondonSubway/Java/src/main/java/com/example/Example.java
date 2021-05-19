a3750aef7fc604fdd6ef11407add7117f4a0bcf3
Monil Bansal <monil.bansal@fivetran.com>
2021-05-19 14:52:52 +0530

    LondonSubway

--- LondonSubway/Java/src/main/java/com/example/Example.java
+++ LondonSubway/Java/src/main/java/com/example/Example.java
@@ -10,12 +10,11 @@ import java.util.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.Instant;  
 import com.google.gson.*;
 
 
 public class Example implements HttpFunction {
+
   @Override
   public void service(HttpRequest request, HttpResponse response) throws Exception {
     Record res  = apiResponse();
@@ -25,77 +24,74 @@ public class Example implements HttpFunction {
     response.setContentType("application/json");
     out.write(recordJSONString);
   }
-  Record apiResponse() {
-    try{
-      URL url = new URL("https://api.tfl.gov.uk/line/mode/tube/status");
 
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestMethod("GET");
-      conn.connect();
+  Record apiResponse() throws Exception{
+    URL url = new URL("https://api.tfl.gov.uk/line/mode/tube/status");
 
-      //Getting the response code
-      int responsecode = conn.getResponseCode();
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.setRequestMethod("GET");
+    conn.connect();
 
-      String inline = "";
-      Scanner scanner = new Scanner(url.openStream());
-    
-      //Write all the JSON data into a string using a scanner
-      while (scanner.hasNext()) {
-          inline += scanner.nextLine();
-      }
-      
-      //Close the scanner
-      scanner.close();
-      
+    //Getting the response code
+    int responsecode = conn.getResponseCode();
 
-      JSONParser parse = new JSONParser();
-      JSONArray data = (JSONArray) parse.parse(inline);
-      Object since =  ((JSONObject) data.get(0)).get("id");
-      List<Map<String,Object> > timeline = new ArrayList<>();
- 
-      for(int i = 0;i<data.size();i++){
-        JSONObject sub = (JSONObject) data.get(i);
-        Map<String,Object> tempMap = new HashMap<>();
-        tempMap.put("linename",sub.get("id"));
-        tempMap.put("linestatus",((JSONObject) ((JSONArray) sub.get("lineStatuses")).get(0)).get("statusSeverityDescription"));
-        tempMap.put("timestamp",sub.get("created"));
-        timeline.add(tempMap);
-      }
-
-      
-      Map<String, List<Map<String, Object> > > insert = new HashMap<>();
-      insert.put("tflLineStatus",timeline);
-
-      Map<String, Map<String, List<String> > > schema = new HashMap<>();
-      Map<String, List<String> > pk = new HashMap<>();
-      List<String> pkEntry = new ArrayList<>();
-      pkEntry.add("linename");
-      pk.put("primary_key",pkEntry );
-      schema.put("tflLineStatus",pk);
-      Map<String, Object> state = new HashMap<>();
-      state.put("since_id",since);
-      Record record = new Record(since, insert, schema, state);
-      return record;
-
-    }catch(Exception e){
-      return new Record();
+    String inline = "";
+    Scanner scanner = new Scanner(url.openStream());
+  
+    //Write all the JSON data into a string using a scanner
+    while (scanner.hasNext()) {
+        inline += scanner.nextLine();
     }
     
-  }
+    //Close the scanner
+    scanner.close();
+    
+    JSONParser parse = new JSONParser();
+    JSONArray data = (JSONArray) parse.parse(inline);
+    Object since =  ((JSONObject) data.get(0)).get("id");
+    List<Map<String,Object> > timeline = new ArrayList<>();
 
-  public class Record {
+    for(int i = 0;i<data.size();i++){
+      JSONObject sub = (JSONObject) data.get(i);
+      Map<String,Object> value = new HashMap<>();
+      value.put("linename",sub.get("id"));
+      value.put("linestatus",((JSONObject) ((JSONArray) sub.get("lineStatuses")).get(0)).get("statusSeverityDescription"));
+      value.put("timestamp",sub.get("created"));
+      timeline.add(value);
+    }
+    
+    // insert
+    Map<String, List<Map<String, Object> > > insert = new HashMap<>();
+    insert.put("tflLineStatus",timeline);
+
+    // schema
+    Map<String, Map<String, List<String> > > schema = new HashMap<>();
+    Map<String, List<String> > primary_key_map = new HashMap<>();
+    List<String> primary_key_list = new ArrayList<>();
+    primary_key_list.add("linename");
+    primary_key_map.put("primary_key",primary_key_list );
+    schema.put("tflLineStatus",primary_key_map);
+
+    //state
+    Map<String, Object> state = new HashMap<>();
+    state.put("since_id",since);
+
+    Record record = new Record(since, insert, schema, state);
+    return record;
+
+  }
+  
+  private class Record {
     public Boolean hasMore;
     public Map<String, List<Map<String, Object> > > insert;
     public Map<String, Map<String, List<String> > > schema;
     public Map<String, Object> state;
+
     public Record(Object since, Map<String, List<Map<String, Object> > > insert, Map<String, Map<String, List<String> > > schema, Map<String, Object> state ) {
       this.hasMore = false;
       this.insert = insert;
       this.schema = schema;
       this.state = state;
-    }
-    public Record(){
-      
     }
   }
 }
