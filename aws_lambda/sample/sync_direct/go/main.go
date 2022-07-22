5d15e09dc4aa52457fa22552ef36e69a659e5a64
ParagPatil96 <parag@fivetran.com>
2022-07-22 12:27:47 +0530

    feature(aws_lambda): added s3 support in go sample

--- aws_lambda/sample/sync_direct/go/main.go
+++ aws_lambda/sample/sync_direct/go/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
 type Request struct {
 	State   map[string]string `json:"state"`
 	Secrets map[string]string `json:"secrets"`
 }
+
 type Response struct {
 	State   map[string]string   `json:"state"`
 	Insert  map[string][]Record `json:"insert"`
@@ -14,15 +16,18 @@ type Response struct {
 	Schema  map[string]Key      `json:"schema"`
 	HasMore bool                `json:"hasMore"`
 }
+
 type Key struct {
 	PrimaryKey []string `json:"primary_key"`
 }
+
 type Record struct {
 	Date     string `json:"date"`
 	OrderId  int    `json:"order_id"`
 	Amount   string `json:"amount"`
 	Discount string `json:"discount"`
 }
+
 func handleLambdaEvent(request Request) (Response, error) {
 	insertTransactions, deleteTransactions, newTransactionsCursor := apiResponse(request.State, request.Secrets)
 	newState := make(map[string]string)
@@ -34,6 +39,7 @@ func handleLambdaEvent(request Request) (Response, error) {
 	transactionsSchema["transactions"] = Key{PrimaryKey: primary_key}
 	return Response{State: newState, Insert: insertTransactions, Delete: deleteTransactions, Schema: transactionsSchema, HasMore: false}, nil
 }
+
 func apiResponse(state map[string]string, secrets map[string]string) (map[string][]Record, map[string][]Record, string) {
 	insertTransactions := make(map[string][]Record)
 	insertTransactions["transactions"] = append(insertTransactions["transactions"], Record{Date: "2017-12-31T05:12:05Z", OrderId: 1001, Amount: "$1200", Discount: "$12"})
@@ -42,6 +48,7 @@ func apiResponse(state map[string]string, secrets map[string]string) (map[string
 	deleteTransactions["transactions"] = append(deleteTransactions["transactions"], Record{Date: "2017-12-31T05:12:05Z", OrderId: 1001, Amount: "$1200", Discount: "$12"})
 	return insertTransactions, deleteTransactions, "2018-01-01T00:00:00Z"
 }
+
 func main() {
 	lambda.Start(handleLambdaEvent)
 }
--- aws_lambda/sample/sync_though_cloud_storage/go/go.mod
+++ aws_lambda/sample/sync_though_cloud_storage/go/go.mod
@@ -2,4 +2,11 @@ module go
 
 go 1.18
 
-require github.com/aws/aws-lambda-go v1.32.1 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.32.1 // indirect
+	github.com/aws/aws-sdk-go v1.44.60 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
